fbd55a872f0ced0eebc1fc88fc79323bbfbd9c5f
Abubakarsimba <bakarsimba@github.com>
2018-07-12 13:47:29 +0300

    ratings

--- Ratings.R
+++ Ratings.R
@@ -1,3 +1,10 @@
-
-file <- read.csv(file.choose(), T, ",")
-hist(file$reviews.rating, main = "Distribution of Product Rating", xlab = "Rating", col = "Yellow")
+library(ggplot2)
+#import the dataset 
+data2<-read.csv(file.choose(),header=TRUE)
+#plotting the graph
+ggplot(data=data2,aes(x=reviews.rating))+
+  geom_histogram(bins=40)+
+  labs(title="Distribution of product ratings",
+       x="Review ratings",
+       y="Number of products"
+       )
